a8b4c2a3092062397a05247767e6a1dc3094a8df
paulgramming <paulgramming@localhost>
2012-06-20 21:29:31 +0100

    update PackMan.java after episode 4

--- Eclipse/PackMan/src/game/packman/PackMan.java
+++ Eclipse/PackMan/src/game/packman/PackMan.java
@@ -1,13 +1,12 @@
 package game.packman;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
@@ -30,38 +29,37 @@ public class PackMan extends Game {
 	int columns, rows;
 	
 	ArrayList<String> lines = new ArrayList<String>();
+	BufferedImage[] mazeImages = new BufferedImage[4];
+	int mazeNo = 0;
+	Maze[] mazes = new Maze[4];
+	char[][] cells; // copy of chars in current maze
 	
 	public PackMan() {
-		// load the lines
-		try {
-			Scanner s = new Scanner(new File("maze.txt"));
-			int r = 0;
-			while (s.hasNextLine()) {
-				String line = s.nextLine();
-				lines.add(line);
-				if (line.contains("5")) {
-					row = r;
-					column = line.indexOf('5');
-				}
-				r++;
-			}
-			s.close();
-			
-			rows = lines.size();
-			columns = lines.get(0).length();
-			
-			width = columns*STEP;
-			height = rows*STEP;
-			
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
+		// load mazes information
+		for (int m=0; m<4; m++) {
+			mazes[m] = new Maze(m);
 		}
+		
+		// update the information from mazes[mazeNo]
+		// get copy cells
+		cells = mazes[mazeNo].getCells();
+		rows = mazes[mazeNo].rows;
+		columns = mazes[mazeNo].columns;
+		// initial position for packman
+		row = mazes[mazeNo].row;
+		column = mazes[mazeNo].column;
+		// size of the game screen
+		width = mazes[mazeNo].width;
+		height = mazes[mazeNo].height;
+		
 		title = "PackMan";
 		frame = 0;
 		curDir = reqDir = KeyEvent.VK_RIGHT;
-//		width = height = 400;
 		try {
 			packman = ImageIO.read(new File("images/packman.png"));
+			for (int m=0; m<4; m++) {
+				mazeImages[m] = ImageIO.read(new File("images/"+m+m+".png"));
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -87,6 +85,15 @@ public class PackMan extends Game {
 		} else {
 			move(curDir);
 		}
+		// updating the pills
+		if (cells[row][column] == '2') {
+			// eat the pill
+			cells[row][column] = '1';
+		} else if (cells[row][column] == '3') {
+			// eat the power pill
+			cells[row][column] = '1';
+			delay = 15;
+		}
 	}
 	
 	static int SUCCESS = 1, FAIL = 0;
@@ -95,25 +102,30 @@ public class PackMan extends Game {
 		// current position of packman is (row, column)
 		switch (reqDir) {
 		case KeyEvent.VK_LEFT: // 37
-			if (column > 0 && charAt(row, column-1) != '0') {
+			if (column > 0 && mazes[mazeNo].charAt(row, column-1) != '0') {
 				column -= 1;
+				return SUCCESS;
+			} 
+			if (column == 0 && cells[row][columns-1] == '1' ) {
+//				row = row;
+				column = columns-1;
 				return SUCCESS;
 			}
 			break;
 		case KeyEvent.VK_UP:   // 38
-			if (row > 0 && charAt(row-1, column) != '0') {
+			if (row > 0 && mazes[mazeNo].charAt(row-1, column) != '0') {
 				row -= 1;
 				return SUCCESS;
 			}
 			break;
 		case KeyEvent.VK_RIGHT: // 39
-			if (column < columns-1 && charAt(row, column+1) != '0') {
+			if (column < columns-1 && mazes[mazeNo].charAt(row, column+1) != '0') {
 				column += 1;
 				return SUCCESS;
 			}
 			break;
 		case KeyEvent.VK_DOWN:  // 40
-			if (row < rows-1 && charAt(row+1, column) != '0') {
+			if (row < rows-1 && mazes[mazeNo].charAt(row+1, column) != '0') {
 				row += 1;
 				return SUCCESS;
 			}
@@ -122,16 +134,18 @@ public class PackMan extends Game {
 		return FAIL;
 	}
 
-	private char charAt(int row, int column) {
-		return lines.get(row).charAt(column);
-	}
-
 	@Override
 	public void draw(Graphics2D g) {
-		for (int r=0; r<rows; r++) {
-			for (int c=0; c<columns; c++) {
-				if (charAt(r,c) != '0') {
-					g.fillRect(c*STEP-14, r*STEP-14, 28, 28);
+		g.drawImage(mazeImages[mazeNo], 0, 0, null);
+		g.setColor(Color.white);
+		for (int r=0; r<mazes[mazeNo].rows; r++) {
+			for (int c=0; c<mazes[mazeNo].columns; c++) {
+				if (cells[r][c] == '2') {
+					// draw pill
+					g.fillOval(c*STEP-3, r*STEP-3, 6, 6);
+				} else if (cells[r][c] == '3') {
+					// draw power pill
+					g.fillOval(c*STEP-6, r*STEP-6, 12, 12);
 				}
 			}
 		}
