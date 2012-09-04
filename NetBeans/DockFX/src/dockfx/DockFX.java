dcf46c43700fc4eff57f144a17174ee5c9c9686b
paulgramming <paulgramming@localhost>
2012-09-04 12:54:28 +0100

    Update DockFX - show how to open home folder & internet when user click.

--- NetBeans/DockFX/src/dockfx/DockFX.java
+++ NetBeans/DockFX/src/dockfx/DockFX.java
@@ -4,6 +4,10 @@
  */
 package dockfx;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -15,6 +19,7 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javax.swing.JFileChooser;
 
 /**
  *
@@ -28,7 +33,7 @@ public class DockFX extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) {
 		stage.setTitle("Hello World!");
@@ -38,8 +43,7 @@ public class DockFX extends Application {
 		final ImageView dock = new ImageView(new Image("images/dock1.png"));
 		dock.setTranslateX(100);
 		dock.setTranslateY(200);
-		dock.setOnMouseClicked(new EventHandler<MouseEvent>(){
-
+		dock.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent arg0) {
 				if (arg0.getButton() == MouseButton.PRIMARY) {
@@ -48,12 +52,11 @@ public class DockFX extends Application {
 					dock.setImage(new Image("images/dock2.png"));
 				}
 			}
-			
 		});
 		root.getChildren().add(dock);
 
 		final String[] images = {
-			"Browser-32.png", 
+			"Browser-32.png",
 			"Internet-32.png",
 			"Mail-32.png",
 			"User-32.png"
@@ -61,9 +64,16 @@ public class DockFX extends Application {
 
 		HBox box = new HBox(20);
 		for (int i = 0; i < images.length; i++) {
-			BouncingIcon icon = new BouncingIcon(new Image("icons/"+images[i]));
+			BouncingIcon icon = new BouncingIcon(new Image("icons/" + images[i]));
 			icon.setEffect(new Reflection());
 			box.getChildren().add(icon);
+			final String action = images[i].split("-")[0].toLowerCase();
+			icon.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent t) {
+					mouseClicked(action);
+				}
+			});
 		}
 
 		box.setTranslateX(150);
@@ -72,5 +82,32 @@ public class DockFX extends Application {
 
 		stage.setScene(new Scene(root, 550, 300));
 		stage.show();
+	}
+
+	public void mouseClicked(String action) {
+		switch (action) {
+			case "user":
+				try {
+					// open user home
+//					System.out.println(System.getenv().keySet().toString());
+					System.out.println("HOME = " + System.getenv("HOME"));
+					Desktop.getDesktop().open(new File(System.getenv("HOME")));
+				} catch (IOException ex) {
+					// No implementation for your OS
+					// fallback
+					JFileChooser chooser = new JFileChooser(new File(System.getenv("HOME")));
+					if ( chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+						File selectedFile = chooser.getSelectedFile();
+						// do something with selectedFile
+					}
+				}
+				break;
+			case "internet":
+				try {
+					Desktop.getDesktop().browse(new URI("http://www.google.com"));
+				} catch (Exception ex) {
+				}
+				break;
+		}
 	}
 }
